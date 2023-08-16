b201f18c878d096e84b0c285e36ae3191d74a6f8
roman <romanze1706@gmail.com>
2023-08-16 20:04:57 +0300

    Добавлены необходимые entity сущности и проведены необходимые миграции

--- src/main/java/com/service/post/entity/PostDelivery.java
+++ src/main/java/com/service/post/entity/PostDelivery.java
@@ -1,7 +1,42 @@
 package com.service.post.entity;
 
-import lombok.Data;
+import com.service.post.entity.enums.TypeOfPostDelivery;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
 
 @Data
-public class PostDelivery{
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "posts")
+@ToString(exclude = "statuses")
+public class PostDelivery {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    long id;
+
+    @Column(name = "type_of_posts", length = 15, nullable = false)
+    @Enumerated(EnumType.STRING)
+    TypeOfPostDelivery typeOfPost;
+
+    @Column(name = "index_recieved", nullable = false, length = 8)
+    String indexOfRecieved;
+
+    @Column(name = "address_recieved", nullable = false, length = 50)
+    String addressOfRecieved;
+
+    @Column(name = "name", nullable = false, length = 20)
+    String name;
+
+    @OneToMany(fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL)
+    @JoinColumn(name = "status_id")
+    List<PostStatus> statuses;
+
 }
